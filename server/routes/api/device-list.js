fdb6e24cd369e6434332da1cd631369baaa09def
Eldar Khaitov <e.khaitov@andersenlab.com>
2018-02-09 01:43:50 +0200

    Can update usb dictionary

--- server/routes/api/device-list.js
+++ server/routes/api/device-list.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const usb = require('usb');
+const usbDetect = require('usb-detection');
+
 
 router.get('/', function(req, res) {
     res.json(usb.getDeviceList());
+    // usbDetect.find(function(err, devices) {
+    //     if(err) {
+    //         res.status(500).send(err);
+    //     } else {
+    //         res.send(devices);
+    //     }
+    // });
 });
 
 module.exports = router;
--- server/services/usbService.js
+++ server/services/usbService.js
@@ -1,10 +1,32 @@
 const usb = require('usb');
+const usbDetect = require('usb-detection');
 const clientService = require('./../services/clientService');
+const http = require('http');
+const fs = require('fs');
+const file = fs.createWriteStream('./server/dictionary/dic');
+
+http.get('http://www.linux-usb.org/usb.ids', function (response) {
+    console.log('USB dictionary was updated');
+    response.pipe(file);
+}).on('error', function (err) { // Handle errors
+    fs.unlink('./server/dictionary/dic'); // Delete the file async. (But we don't check the result)
+    console.log('Error when updating USB dictionary');
+});
 
 function watch() {
+    usbDetect.startMonitoring();
+    usbDetect.on('change', device => {
+        // clientService.sendToAll(device);
+    });
     usb.on('attach', device => {
-        clientService.sendToAll(device);
+        device.open();
+        clientService.sendToAll(device.interfaces);
     })
 }
 
-module.exports = {watch};
+function unWatch() {
+    usbDetect.stopMonitoring();
+}
+
+
+module.exports = {watch, unWatch};
