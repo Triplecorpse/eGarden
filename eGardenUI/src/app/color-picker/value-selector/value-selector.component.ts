9b9c2731cc2752dd77ee0b33f266af635c835752
Eldar Khaitov <e.khaitov@andersenlab.com>
2018-04-11 00:09:34 +0300

    Created component for selecting color

--- eGardenUI/src/app/color-picker/value-selector/value-selector.component.ts
+++ eGardenUI/src/app/color-picker/value-selector/value-selector.component.ts
@@ -1,5 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {log} from "util";
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+interface iCustomEvent {
+  name: string,
+  source: ElementRef,
+  value: number
+}
 
 @Component({
   selector: 'app-value-selector',
@@ -9,14 +14,20 @@ import {log} from "util";
 export class ValueSelectorComponent implements OnInit {
   @Input() label: string;
   @Input() gradient: string;
+  @Input() name?: string;
+  @Output() input: EventEmitter<iCustomEvent> = new EventEmitter();
   private pointerLeft: number;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   selectValue(event) {
-    if (event.type === 'click' || (event.type === 'mousemove' && event.buttons === 1)) {
-      console.log(event);
+    if ((event.type === 'click' || (event.type === 'mousemove' && event.buttons === 1)) && !event.target.className.includes('picker__pointer')) {
       this.pointerLeft = event.offsetX;
+      this.input.emit({
+        name: this.name,
+        source: this.elementRef.nativeElement,
+        value: (100 / event.target.offsetWidth) * event.offsetX
+      });
     }
   }
 
