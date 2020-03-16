04dcf41daea65edb4684e3efa4bc8e47df3760f4
Eldar Khatov <eldar.khaitov@gmail.com>
2020-03-16 19:20:24 +0200

    Init the relay

--- server/light.service.ts
+++ server/light.service.ts
@@ -1,19 +1,22 @@
-import {Relay, Board} from 'johnny-five';
+import {Relay, Board, Led} from 'johnny-five';
 
 export class Light {
     private board = new Board({port: 'COM3'});
     private relay1: Relay;
     private relay2: Relay;
+    private led: Led;
 
     constructor() {
         this.board.on('ready', () => {
             this.relay1 = new Relay({pin: 10, type: 'NC'});
             this.relay2 = new Relay({pin: 9, type: 'NC'});
+            this.led = new Led(11);
 
             setInterval(() => {
                 this.relay1.toggle();
                 this.relay2.toggle();
-            }, 50)
+                this.led.toggle();
+            }, 500)
         });
     }
 }
